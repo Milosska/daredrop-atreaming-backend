3a6381375bb0fd2d17dafb4cca9d46629b956ca3
Milosska <110258611+Milosska@users.noreply.github.com>
2023-06-30 01:38:38 +0300

    adds rating virtual field to streamer model

--- models/Streamer.js
+++ models/Streamer.js
@@ -51,8 +51,16 @@ const schema = new Schema(
   {
     versionKey: false,
     timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
 );
+
+schema.virtual("rating").get(function () {
+  const ratingRatio = this.upvote / (this.upvote + this.downvote);
+  const rating = Math.round(ratingRatio * 5 * 100) / 100;
+  return rating;
+});
 
 schema.post("save", handleMongooseError);
 
